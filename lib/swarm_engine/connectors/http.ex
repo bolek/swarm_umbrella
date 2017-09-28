c24d4afb9b5667db4cd00cf0a3fa2014032aeb9b
bolek <bolek@alumni.cmu.edu>
2017-09-27 23:56:08 -0400

    Move HTTP error to separate file

--- lib/swarm_engine/connectors/http.ex
+++ lib/swarm_engine/connectors/http.ex
@@ -1,26 +1,10 @@
 defmodule SwarmEngine.Connectors.HTTP do
-  alias __MODULE__.Helpers
+  alias __MODULE__.{Helpers, Error}
 
   @http Application.get_env(:swarm_engine, :http_client)
 
   def create(params, options \\ []) do
     {__MODULE__, params, options}
-  end
-
-  defmodule Error do
-    alias __MODULE__
-
-    defexception [:message]
-
-    def exception({url, reason}) do
-      msg = "requesting #{url}, got: #{Kernel.inspect(reason)}"
-      %Error{message: msg}
-    end
-
-    def exception(url) do
-      msg = "error when requesting #{url}"
-      %Error{message: msg}
-    end
   end
 
   def request({__MODULE__, %{url: url}, opts}) do
