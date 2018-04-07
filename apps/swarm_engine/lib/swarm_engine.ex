20f3c9eda51f7b5c525cf3d4273e8653b17dcbcd
bolek <boleslaw.kurowski@gmail.com>
2018-04-07 17:16:49 -0400

    Remove SwarmEngine dataset interface We will be interacting with the database differently

--- apps/swarm_engine/lib/swarm_engine.ex
+++ apps/swarm_engine/lib/swarm_engine.ex
@@ -1,9 +1,4 @@
 defmodule SwarmEngine do
-  alias SwarmEngine.Repo
-  alias SwarmEngine.Repo.Schema.Dataset
-
-  import Ecto.Query, only: [from: 2]
-
   @moduledoc """
   Documentation for SwarmEngine.
   """
@@ -19,35 +14,5 @@ defmodule SwarmEngine do
   """
   def hello do
     :world
-  end
-
-  def create_dataset(attrs \\ %{}) do
-    changeset = %Dataset{store: nil}
-    |> Dataset.new_changeset(attrs)
-
-    case changeset.valid? do
-      true ->
-        changeset
-        |> Ecto.Changeset.apply_changes
-      false ->
-        changeset
-    end
-    |> Repo.insert
-
-  end
-
-  def list_datasets(), do:
-    from(d in Dataset, preload: [:tracker]) |> Repo.all
-
-  def get_dataset!(id), do:
-    from(d in Dataset, preload: [:tracker]) |> Repo.get!(id)
-
-  def delete_dataset(%Dataset{} = dataset), do:
-    Repo.delete(dataset)
-
-  def update_dataset(%Dataset{} = dataset, attrs) do
-    dataset
-    |> Dataset.update_changeset(attrs)
-    |> Repo.update()
   end
 end
--- apps/swarm_engine/test/swarm_engine_test.exs
+++ apps/swarm_engine/test/swarm_engine_test.exs
@@ -6,48 +6,4 @@ defmodule SwarmEngineTest do
   test "greets the world" do
     assert SwarmEngine.hello() == :world
   end
-
-  @valid_attrs %{
-    "name" => "My Dataset",
-    "source" => %{
-      "type" => "LocalFile",
-      "path" => "tmp.txt"
-    },
-    "decoder" => %{
-      "type" => "CSV",
-      "headers" => true,
-      "separator" => ",",
-      "delimiter" => "/n"
-    }
-  }
-
-  alias SwarmEngine.Repo.Schema.Dataset
-
-  def dataset_fixture(attrs \\ %{}) do
-    {:ok, dataset} =
-      attrs
-      |> Enum.into(@valid_attrs)
-      |> SwarmEngine.create_dataset
-
-    dataset
-  end
-
-  test "create_dataset/1 with valid data creates a dataset" do
-    assert {:ok, %Dataset{}} = SwarmEngine.create_dataset(@valid_attrs)
-  end
-
-  test "get_dataset/1 returns dataset with given id" do
-    dataset = dataset_fixture()
-    assert SwarmEngine.get_dataset!(dataset.id) == dataset
-  end
-
-  test "list_datasets/0 returns empty array when no datasets" do
-    assert SwarmEngine.list_datasets() == []
-  end
-
-  test "list_datasets/0 returns list of existing datasets" do
-    dataset_1 = dataset_fixture()
-    dataset_2 = dataset_fixture()
-    assert SwarmEngine.list_datasets() == [dataset_1, dataset_2]
-  end
 end
